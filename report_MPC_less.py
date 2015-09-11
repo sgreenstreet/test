92634391f26c6a3b07c7bd38151fd302b18c2af8
Sarah Greenstreet <sgreenstreet@lcogt.net>
2015-09-11 11:20:04 -0700

    Moved code into function, added data file

--- report_MPC_less.py
+++ report_MPC_less.py
@@ -6,19 +6,22 @@ in addition to rounding the magnitude values to 0.1 mag.
 
 import sys
 
-num_line = 0
+def summarize_MPC_report(f):
+    num_line = 0
+    for line in f:
+	    if 'V37' in line:
+		    num_line += 1
+		    if num_line % 8 == 0:
+			    part1 = line[0:65]
+			    mag = line[65:70]
+			    part3 = line[70:]
+			    mag_num = float(mag)
+			    mag_num_round = round(mag_num, 1)
+    #			print line,
+			    print "%s%.1f %s"%(part1, mag_num_round, part3),
+    return
 
-f = open('2003_RB.dat')
+MPC_file = open('2003_RB.dat')
 #f = open(sys.argv[1],'r')
-for line in f:
-	if 'V37' in line:
-		num_line += 1
-		if num_line % 8 == 0:
-			part1 = line[0:65]
-			mag = line[65:70]
-			part3 = line[70:]
-			mag_num = float(mag)
-			mag_num_round = round(mag_num, 1)
-#			print line,
-			print "%s%.1f %s"%(part1, mag_num_round, part3),
-f.close()
+summarize_MPC_report(MPC_file)
+MPC_file.close()
