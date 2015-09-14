f32a30a1bbad9d9da5932d268081a8b0410b77e1
Sarah Greenstreet <sgreenstreet@lcogt.net>
2015-09-14 09:47:21 -0700

    MPC lines put into list before printing

--- report_MPC_less.py
+++ report_MPC_less.py
@@ -6,24 +6,27 @@ in addition to rounding the magnitude values to one decimal place.
 
 import sys
 
+report_line = []
+
 def summarize_MPC_report(f):
     num_line = 0
     for line in f:
-	    if 'V37' in line:
-		    num_line += 1
-		    if num_line % 8 == 0:
-			    part1 = line[0:65]
-			    mag = line[65:70]
-			    part3 = line[70:]
-			    mag_num = float(mag)
-			    mag_num_round = round(mag_num, 1)
-    #			print line,
-			    print "%s%.1f %s"%(part1, mag_num_round, part3),
-    return
-
+        if 'V37' in line:
+            num_line += 1
+            if num_line % 8 == 0:
+                part1 = line[0:65]
+                mag = line[65:70]
+                part3 = line[70:]
+                mag_num = float(mag)
+                mag_num_round = round(mag_num, 1)
+                report_line.append([part1, mag_num_round, part3.rstrip()])
+    return report_line
 
 MPC_file = open(sys.argv[1],'r')
 
 summarize_MPC_report(MPC_file)
 
 MPC_file.close()
+
+for i in range(len(report_line)):
+    print "%s%.1f %s" % (report_line[i][0], report_line[i][1], report_line[i][2])
